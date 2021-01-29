a5abb335d9f661beb17e78cc93bee12a3208cd70
crackoscl <gilbert.ata@gmail.com>
2021-01-29 15:24:04 -0300

    modificando titulos ramdom

--- djanguito.py
+++ djanguito.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import datetime
 import mimetypes
+import random
 
 PORT = 8080
 
@@ -16,6 +17,22 @@ def get_static(file_name):
         static_file = file.read()
         return static_file, mime
     
+def titulos():
+    titulo = random.choice([
+                'Conferencia de prensa del presidente',
+                'Introducción a Flickr 2.0 ',
+                'Ponga un tigre en su tanque',
+                'Confiesa Angelina Jol',
+                'Mi entrevista exclusiva con Steve Jobs',
+                '¿Quién más quiere hacerse rico online?',
+                'Rami Malek comparte una emotiva historia sobre Robin Williams',
+                'Gal Gadot recordó a las víctimas del Holocausto: "Fui a encender una vela en el muro de Auchwitz en recuerdo de mi abuelo"',
+                'Muere el destacado cantautor argentino César Isella'
+                ])
+    return titulo
+    
+    
+    
 class myHandler(BaseHTTPRequestHandler):
     
     #Handler for the GET requests
@@ -25,8 +42,11 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type','text/html')
             self.end_headers()
             # Send the html message
+            
             context = {
-                'titulo':'esta es la pagina renderizada',
+                'titulo1':titulos(),
+                'titulo2':titulos(),
+                'titulo3':titulos(),
                 'hora':str(datetime.datetime.now()),
                 'saludo':'Zdrastvuytie'
             }
@@ -36,17 +56,32 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
+            context = {
+                'titulo':'Nuestra Empresa',
+                'parrafo':'''Somos uno de los más grandes y prestigiosos 
+                conglomerados de retail en América Latina. Contamos con operaciones activas en Argentina, Brasil, Chile, Perú y Colombia, 
+                donde día a día desarrollamos una exitosa estrategia multiformato que hoy da 
+                trabajo a más de 140 mil colaboradores'''
+            }
             # Send the html message
-            self.wfile.write(("<b>Empresa</b>"
-                        + "<br><br> La hora actual es:" + str(datetime.datetime.now())).encode('utf-8'))
+            self.wfile.write(render('empresa.html',context))
 
         elif self.path == '/contacto':
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
             # Send the html message
-            self.wfile.write(("<b>Contacto</b>"
-                        + "<br><br> La hora actual es:" + str(datetime.datetime.now())).encode('utf-8'))
+            
+            
+            context = {
+                'titulo':'formulario de contacto',
+                'direccion': '''Vicuña Mackenna 4230, Macul, RM, Chile''',
+                'nombre':'nombre',
+                'correo':'correo',
+                'mensaje':'mensaje',
+                
+            }
+            self.wfile.write(render('contacto.html',context))
 
         elif self.path == '/static/img/Putin.jpeg':
             content, mime = get_static('static/img/Putin.jpeg')
@@ -55,6 +90,16 @@ class myHandler(BaseHTTPRequestHandler):
             self.end_headers()
             # Send the html message
             self.wfile.write(content)
+        
+        elif self.path == '/static/css/style.css':
+            content, mime = get_static('static/css/style.css')
+            self.send_response(200)
+            self.send_header('Content-type',mime)
+            self.end_headers()
+            # Send the html message
+            self.wfile.write(content)
+        
+        
             
             
 server = HTTPServer(('', PORT), myHandler)
